6e4809879651baa3fe59c906f6bf421277cc8155
RezzAja <f4khrurrozy.22nkh@gmail.com>
2026-03-02 22:16:46 +0700

    penambahan konstruktor mahasiswa

--- p2/Mahasiswa.java
+++ p2/Mahasiswa.java
@@ -4,6 +4,19 @@ public class Mahasiswa {
     String kelas;
     double ipk;
 
+    
+    public Mahasiswa() {
+       
+    }
+
+    
+    public Mahasiswa(String nm, String nim, double ipk, String kls) {
+        this.nama  = nm;
+        this.nim   = nim;
+        this.ipk   = ipk;
+        this.kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
@@ -15,7 +28,6 @@ public class Mahasiswa {
         kelas = kelasBaru;
     }
 
-    
     void updateIpk(double ipkBaru) {
         if (ipkBaru < 0.0 || ipkBaru > 4.0) {
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
