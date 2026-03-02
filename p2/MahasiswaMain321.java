4555e1a5dac94d23446d970cef3b89b089d8b191
RezzAja <f4khrurrozy.22nkh@gmail.com>
2026-03-02 22:18:00 +0700

    konstraktor mahasiswa main

--- p2/MahasiswaMain321.java
+++ p2/MahasiswaMain321.java
@@ -1,7 +1,6 @@
-
-
 public class MahasiswaMain321 {
     public static void main(String[] args) {
+        
         Mahasiswa mhs1 = new Mahasiswa();
         mhs1.nama  = "Muhammad Ali Farhan";
         mhs1.nim   = "2241720171";
@@ -12,5 +11,15 @@ public class MahasiswaMain321 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+       
+        Mahasiswa mhs2 = new Mahasiswa(
+            "Annisa Nabila",
+            "2241720160",
+            3.25,
+            "TI 2L"
+        );
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
