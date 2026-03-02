0c0d5b64381ba2270b9444ddcbd36ab2a3446188
RezzAja <f4khrurrozy.22nkh@gmail.com>
2026-03-02 21:42:58 +0700

    tambah ipk percobaan 2

--- p2/Mahasiswa123.java
+++ p2/Mahasiswa123.java
@@ -16,7 +16,11 @@ public class Mahasiswa123 {
     }
 
     void updateIpk(double ipkBaru) {
-        ipk = ipkBaru;      
+        if (ipkBaru < 0.0 || ipkBaru > 4.0) {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        } else {
+            ipk = ipkBaru;
+        }
     }
 
     String nilaiKinerja() {
