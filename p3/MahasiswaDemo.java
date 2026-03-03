2b6349f02c8dcd23cc9a3501acfc84ca05a0e589
RezzAja <f4khrurrozy.22nkh@gmail.com>
2026-03-03 22:16:24 +0700

    penambahan mahasiswademo p3

---
+++ p3/MahasiswaDemo.java
@@ -0,0 +1,41 @@
+package p3;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Mahasiswa[] arrayOfMahasiswa = new Mahasiswa[3];
+        String dummy;
+
+        for (int i = 0; i < 3; i++) {
+            arrayOfMahasiswa[i] = new Mahasiswa();
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM    : ");
+            arrayOfMahasiswa[i].nim = sc.nextLine();
+            System.out.print("Nama   : ");
+            arrayOfMahasiswa[i].nama = sc.nextLine();
+            System.out.print("Kelas  : ");
+            arrayOfMahasiswa[i].kelas = sc.nextLine();
+            System.out.print("IPK    : ");
+            dummy = sc.nextLine();
+            try {
+                arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            } catch (NumberFormatException e) {
+                arrayOfMahasiswa[i].ipk = 0;
+            }
+            System.out.println("------------------------------");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+            System.out.println("NIM   : " + arrayOfMahasiswa[i].nim);
+            System.out.println("Nama  : " + arrayOfMahasiswa[i].nama);
+            System.out.println("Kelas : " + arrayOfMahasiswa[i].kelas);
+            System.out.println("IPK   : " + arrayOfMahasiswa[i].ipk);
+            System.out.println("------------------------------");
+        }
+
+        sc.close();
+    }
+}
